3c3cfc6777bd48b114fcb8068319b20217687311
foreverzmy <921255465@qq.com>
2024-07-14 23:28:24 +0800

    chore: update gh-pages script

--- gh-pages.js
+++ gh-pages.js
@@ -1,3 +1,12 @@
+const { execSync } = require('node:child_process');
 const ghpages = require('gh-pages');
 
-ghpages.publish('docs/doc_build', { cname: 'jsoncargo.foreverz.cn' }, console.error);
+execSync('cd docs && npm run build');
+console.log('build docs success.');
+
+ghpages.publish('docs/doc_build', { cname: 'jsoncargo.foreverz.cn' }, (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('publish success.');
+});
